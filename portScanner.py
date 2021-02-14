903841bca7740be357d86e2790f53e7dc81a44ca
trippedego <72950222+trippedego@users.noreply.github.com>
2021-02-14 11:32:56 -0600

    Update and rename NetworkHomework.py to portScanner.py

---
+++ portScanner.py
@@ -0,0 +1,33 @@
+import socket
+
+def scanMe():
+	while True:
+		target = str(input("Enter the target IP (or enter nothing to exit program)..."))
+		if not target:
+			exit()
+		try:
+			socket.inet_aton(target)
+			try:
+				port = int(input("Enter port you wish to scan..."))
+				proto = str(input("TCP or UDP Scan???"))
+				if proto == "TCP" or proto == "tcp":
+					sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+					result = sock.connect((target,port))
+					print("Port {} is OPEN".format(port))
+				elif proto == "UDP" or proto == "udp":
+					sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+					result = sock.connect((target,port))
+					print("Port {} is OPEN".format(port))
+				else:
+					print("You did not specify either TCP or UDP...")
+				continue
+			except:
+				print("Port {} is CLOSED".format(port))
+				continue
+		except:
+			print("Not a valid IP address!!!")
+			exit()
+		
+		return
+
+scanMe()
