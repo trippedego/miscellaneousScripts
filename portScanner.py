51eccc6cb1944646eab2f9a523a5dd10b41979a8
trippedego <72950222+trippedego@users.noreply.github.com>
2021-02-15 10:55:59 -0600

    Update portScanner.py

--- portScanner.py
+++ portScanner.py
@@ -14,28 +14,27 @@ def scanMe():
 				if proto == "TCP" or proto == "tcp":
 					sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 					result = sock.connect((target,port))
-					print("Port {} is OPEN".format(port))
+					print("Port {}:{} is OPEN".format(target,port))
 				elif proto == "UDP" or proto == "udp":
 					interface = str(input("Enter interface to send packet out of... "))
 					ans, unans = sr(IP(dst=target)/UDP(sport=65500, dport=port)/Raw(load='test'), iface=interface, timeout=5, verbose=0)
-					
-					if ans.summary().find("ICMP") != -1:
-						print("Port {} is CLOSED".format(port))
+					tmp = str(ans.summary)
+					icmpHIT = tmp.split(" ")
+					if icmpHIT[-2][-1] == "1":
+						print("Port {}:{} is CLOSED".format(target,port))
 						continue
 					else:
-						print("Port {} is OPEN / FILTERED".format(port))
+						print("Port {}:{} is OPEN / FILTERED".format(target,port))
 						continue
 						
 				else:
 					print("You did not specify either TCP or UDP...")
 				continue
 			except:
-				print("Port {} is CLOSED".format(port))
+				print("Port {}:{} is CLOSED".format(target,port))
 				continue
 		except:
 			print("Not a valid IP address!!!")
 			exit()
 		
 		return
-
-scanMe()
