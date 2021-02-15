a5a671d3cec3b490903735fbdffad214e411936f
trippedego <72950222+trippedego@users.noreply.github.com>
2021-02-15 10:33:14 -0600

    Update portScanner.py

--- portScanner.py
+++ portScanner.py
@@ -3,31 +3,29 @@ from scapy.all import *
 
 def scanMe():
 	while True:
-		target = str(input("Enter the target IP (or enter nothing to exit program)..."))
+		target = str(input("Enter the target IP (or enter nothing to exit program)... "))
 		if not target:
 			exit()
 		try:
 			socket.inet_aton(target)
 			try:
-				port = int(input("Enter port you wish to scan..."))
-				proto = str(input("TCP or UDP Scan???"))
+				port = int(input("Enter port you wish to scan... "))
+				proto = str(input("TCP or UDP Scan??? "))
 				if proto == "TCP" or proto == "tcp":
 					sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 					result = sock.connect((target,port))
 					print("Port {} is OPEN".format(port))
 				elif proto == "UDP" or proto == "udp":
-					timeout = 10
-					ans, unans = sr(IP(dst=target)/UDP(sport=43644, dport=port)/Raw(load='test'),iface="Intel(R) 82574L Gigabit Network Connection",timeout=2, verbose=0)
-					pkt == None
-					if pkt == None:
-						print("Port {} is Open / Filtered".format(port))
+					interface = str(input("Enter interface to send packet out of... "))
+					ans, unans = sr(IP(dst=target)/UDP(sport=65500, dport=port)/Raw(load='test'), iface=interface, timeout=5, verbose=0)
+					
+					if ans.summary().find("ICMP") != -1:
+						print("Port {} is CLOSED".format(port))
+						continue
 					else:
-						if pkt.haslayer(ICMP):
-							print("Port {} is Closed".format(port))
-						elif pkt.haslayer(UDP):
-							print("Port {} is Open".format(port))
-						else:
-							print("Unknown response...")
+						print("Port {} is OPEN / FILTERED".format(port))
+						continue
+						
 				else:
 					print("You did not specify either TCP or UDP...")
 				continue
